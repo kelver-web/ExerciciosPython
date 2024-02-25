037112eefda711053ff5b570995864ffb2e2979e
kelver-web <kelverwt@gmail.com>
2024-02-24 21:02:55 -0300

    exec02

---
+++ EstruturaDeDecisao/exec02.py
@@ -0,0 +1,9 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+valor = int(input('Digite um valor: '))
+
+if valor > 0:
+    print(f'O valor {valor} é positivo')
+else:
+    print(f'O valor {valor} é negativo')
+
