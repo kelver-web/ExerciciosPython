ade5d7c0f11ea0cf30d3197d0ed02909c20c2772
kelver-web <kelverwt@gmail.com>
2024-03-12 20:32:11 -0300

    exec05

--- EstruturaDeDecisao/exec05.py
+++ EstruturaDeDecisao/exec05.py
@@ -4,3 +4,16 @@
 # A mensagem "Reprovado", se a média for menor do que sete;
 # A mensagem "Aprovado com Distinção", se a média for igual a dez.
 
+
+
+nota1 = float(input("Digite a 1º nota:  "))
+nota2 = float(input("Digite a 2º nota:  "))
+
+media = (nota1 + nota2) / 2
+
+if media >= 7 and media < 10:
+    print('Aprovado')
+elif media < 7:
+    print('Reprovado')
+else:
+    print('Aprovado com Distinção')
