99c5993708596f6811ded1cc04236dc15dfad62a
kelver-web <kelverwt@gmail.com>
2024-03-12 20:24:31 -0300

    exec03

---
+++ EstruturaDeDecisao/exec04.py
@@ -0,0 +1,8 @@
+# Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+letra = input("Digite uma letra:  ")
+
+if letra in ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'):
+    print(f'A letra [{letra}] é vogal')
+else:
+    print(f'A letra [{letra}] é consoante')
